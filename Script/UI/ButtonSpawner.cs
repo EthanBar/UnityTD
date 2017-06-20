1f269a8fb387f6e2de0c23219062b717cff21c2b
Ethan Barker <ethanbarker0@gmail.com>
2017-06-19 22:16:06 -0700

    Buttons scale to fit screen width

--- Script/UI/ButtonSpawner.cs
+++ Script/UI/ButtonSpawner.cs
@@ -19,45 +19,53 @@ public class ButtonSpawner : MonoBehaviour {
         activeTower = towers[0];
         activeSpell = spells[0];
         activeEnemy = enemys[0];
-        int x = 60;
+
+        int incAmt = Screen.width / (towers.Length + spells.Length + enemys.Length);
+        int x = incAmt/2;
         foreach (GameObject enemy in enemys) {
             GameObject but = Instantiate(button) as GameObject;
             but.transform.SetParent(transform, false);
             but.transform.GetChild(0).GetComponent<Text>().text = enemy.name;
             but.SetActive(true);
-            but.GetComponent<RectTransform>().position = new Vector2(x, 20f);
+            RectTransform trans = but.GetComponent<RectTransform>();
+            trans.position = new Vector2(x, 20f);
+            trans.sizeDelta = new Vector2(incAmt, 40f);
             Button butComp = but.GetComponent<Button>();
             Image img = but.GetComponent<Image>();
             butComp.onClick.AddListener(delegate { // I love C#
                 SetEnemy(enemy);
             });
-            x += 120;
+            x += incAmt;
         }
         foreach (GameObject tower in towers) {
             GameObject but = Instantiate(button) as GameObject;
             but.transform.SetParent(transform, false);
             but.transform.GetChild(0).GetComponent<Text>().text = tower.name;
             but.SetActive(true);
-            but.GetComponent<RectTransform>().position = new Vector2(x, 20f);
+            RectTransform trans = but.GetComponent<RectTransform>();
+            trans.position = new Vector2(x, 20f);
+            trans.sizeDelta = new Vector2(incAmt, 40f);
             Button butComp = but.GetComponent<Button>();
             Image img = but.GetComponent<Image>();
             butComp.onClick.AddListener(delegate { // I love C#
                 SetTower(tower);
             });
-            x += 120;
+            x += incAmt;
         }
         foreach (GameObject spell in spells) {
             GameObject but = Instantiate(button) as GameObject;
             but.transform.SetParent(transform, false);
             but.transform.GetChild(0).GetComponent<Text>().text = spell.name;
             but.SetActive(true);
-            but.GetComponent<RectTransform>().position = new Vector2(x, 20f);
+            RectTransform trans = but.GetComponent<RectTransform>();
+            trans.position = new Vector2(x, 20f);
+            trans.sizeDelta = new Vector2(incAmt, 40f);
             Button butComp = but.GetComponent<Button>();
             Image img = but.GetComponent<Image>();
             butComp.onClick.AddListener(delegate { // I love C#
                 SetSpell(spell);
             });
-            x += 120;
+            x += incAmt;
         }
     }
 
